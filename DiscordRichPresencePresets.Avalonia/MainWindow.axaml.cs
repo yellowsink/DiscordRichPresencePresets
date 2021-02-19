77f3ebf7b03a7339194097abe19bcc0f03d1e7f5
cainy-a <cainy-a@users.noreply.github.com>
2021-02-19 12:37:33 +0000

    A working~ish UI

--- DiscordRichPresencePresets.Avalonia/MainWindow.axaml.cs
+++ DiscordRichPresencePresets.Avalonia/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace DiscordRichPresencePresets.Avalonia
@@ -12,6 +13,26 @@ namespace DiscordRichPresencePresets.Avalonia
 #if DEBUG
 			this.AttachDevTools();
 #endif
+		}
+		
+		private void AddPresence(object? sender, RoutedEventArgs e)
+		{
+			
+		}
+
+		private void SavePresences(object? sender, RoutedEventArgs e)
+		{
+			
+		}
+
+		private void LoadPresences(object? sender, RoutedEventArgs e)
+		{
+			
+		}
+
+		private void OptionsPopup(object? sender, RoutedEventArgs e)
+		{
+			
 		}
 
 		private void InitializeComponent() { AvaloniaXamlLoader.Load(this); }
